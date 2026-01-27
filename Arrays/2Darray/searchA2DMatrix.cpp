72ecc88ec7bd2eeac68233de4cf0e9c3d649382e
Arshmi-Ansari <arshmiansari@gmail.com>
2026-01-27 13:36:12 +0530

    Create searchA2DMatrix.cpp

---
+++ Arrays/2Darray/searchA2DMatrix.cpp
@@ -0,0 +1,25 @@
+// Leetcode : 74   -->   Search a 2D Matrix
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int s = 0;
+        int e = row*col - 1;
+        while(s <= e){
+            int mid = s + (e - s) / 2;
+            int element = matrix[mid/col][mid%col];
+            if(element == target){
+                return true;
+                break;
+            }
+            else if ( target < element){
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+        return false;
+    }
+};
