5093e803a7ec97fbb212932385f2b580b9b75d37
Arshmi-Ansari <arshmiansari@gmail.com>
2026-01-29 19:06:10 +0530

    Create BuyAndSellStock.cpp

---
+++ Arrays/BuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+// Leetcode : 121  -->  Best time to buy and sell stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int buyPrice = prices[0];
+        int profit = 0;
+        for(int i = 1;i < n; i++){
+            if(prices[i] < buyPrice){
+                buyPrice = prices[i];
+            }
+            profit = max(profit, prices[i] - buyPrice);
+        }
+        return profit;
+    }
+};
