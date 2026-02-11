dbc70a7cb77e79fddc2b817ce1167108623c9498
Arshmi Ansari <arshmiansari@gmail.com>
2026-02-11 07:58:11 +0530

    Create Subsets.cpp

---
+++ Arrays/Subsets.cpp
@@ -0,0 +1,25 @@
+// Leetcode : 78  -->  Subtsets
+class Solution {
+private:
+    void solve(vector<int> nums,vector<int> output,int index, vector<vector<int>>& ans){
+        // base case 
+        if(index >= nums.size()) {
+            ans.push_back(output);
+            return ;
+        }
+        // exclude element
+        solve(nums,output,index+1,ans);
+        // include element
+        int element = nums[index];
+        output.push_back(element);
+        solve(nums,output,index+1,ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums,output,index,ans);
+        return ans;
+    }
+};
