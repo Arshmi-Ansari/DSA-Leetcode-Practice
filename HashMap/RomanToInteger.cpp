9212b8d6d9d1b953353214f0874fb3766e30d542
Arshmi Ansari <arshmiansari@gmail.com>
2026-02-27 20:47:02 +0530

    Create RomanToInteger.cpp

---
+++ HashMap/RomanToInteger.cpp
@@ -0,0 +1,26 @@
+// Leetcode : 13  --> Roman To Integer
+// * Time Complexity : O(n)
+// * Space Complexity : O(1)
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> map;
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+        int ans = 0;
+        for(int i = 0; i< s.size(); i++){
+            if(i+1 < s.size() && map[s[i]] < map[s[i+1]]){
+                ans -= map[s[i]];
+            }
+            else{
+                ans += map[s[i]];
+            }
+        }
+        return ans;
+    }
+};
